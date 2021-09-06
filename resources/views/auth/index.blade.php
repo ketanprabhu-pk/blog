4e116b062c9734b128a33998819aa594796fee56
pketanp <pketanp01@gmail.com>
2021-09-07 00:51:43 +0530

    .

---
+++ resources/views/auth/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('content')
+    <div class="d-flex justify-content-center">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col">Auth</div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-gray-300">
+    <main>
+        <nav class="p-5 px-28 bg-gray-800 text-white flex justify-between">
+            <ul class="flex mx-9 item-center">
+                <li><a href="http://" class="p-3">Home</a></li>
+                <li><a href="http://" class="p-3">Dashboard</a></li>
+                <li><a href="http://" class="p-3">Post</a></li>
+                <li><a href="http://" class="p-3"></a></li>
+            </ul>
+            <ul class="flex mx-9 item-center">
+                <li><a href="http://" class="p-3">Ketan Prabhu</a></li>
+                <li><a href="http://" class="p-3">Login</a></li>
+                <li><a href="http://" class="p-3">Register</a></li>
+                <li><a href="http://" class="p-3">Logout</a></li>
+            </ul>
+        </nav>
+        <div class="container">
+            @yield('content')
+        </div>
+    </main>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>

2b9b83d3a9f22de5c1f919ae1ea369f6192990c6
ketanprabhu97 <ketanprabhu1297@gmail.com>
2021-09-13 23:23:15 +0530

    post

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -15,6 +16,8 @@ class ProfileController extends Controller
 
     public function index()
     {
-        return view('posts.index');
+        $data['user'] = auth()->user();
+        $data['posts'] = auth()->user()->posts()->with(['user', 'likes'])->paginate(20);
+        return view('posts.index', $data);
     }
 }
